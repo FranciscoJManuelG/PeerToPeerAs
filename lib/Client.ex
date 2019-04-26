2a0a614f6fb9f1fa9b9931620a7f2f51b6b241c8
David García Gondell <david.ggondell@udc.es>
2019-04-26 16:45:10 +0200

    [ADD] client updated

--- lib/Client.ex
+++ lib/Client.ex
@@ -1,19 +1,42 @@
 defmodule Client do
-    def send(ip,port,message) do
+    use GenServer
+
+    def connect(ip,port) do
         {:ok,socket} = :gen_tcp.connect(ip,port,[:binary, packet: :line, active: false, reuseaddr: true])
-        :gen_tcp.send(socket,message)
+        {:ok,pid} = GenServer.start_link(__MODULE__, [socket])
+        Process.register(pid,:client)
+        :ok
+    end
+
+    def send(message) do
+        GenServer.cast(:client,{:send,message})
+    end
+
+    def close() do
+        GenServer.cast(:client,:close)
+        GenServer.stop(:client)
+        Process.unregister(:client)
+        :ok
+    end
+
+    defp send_stringlist([],_),do: :ok
+    defp send_stringlist([""|tl], socket),do: send_stringlist(tl,socket)
+    defp send_stringlist([message|tl], socket) do
+        :gen_tcp.send(socket,message<>"\n")
         {:ok, data} = :gen_tcp.recv(socket, 0)
         IO.puts(data)
-        # loop(socket,message,10)
+        send_stringlist(tl,socket)
+    end
+
+    def init(socket),do: {:ok,socket}
+
+    def handle_cast({:send,message},[socket]) do
+        send_stringlist(String.split(message,"\n"),socket)
+        {:noreply,[socket]}
+    end
+
+    def handle_cast(:close,[socket]) do
         :gen_tcp.close(socket)
+        {:noreply,[]}
     end
-
-    defp loop(_,_,0),do: :ok
-    defp loop(socket,message,n) do 
-        :gen_tcp.send(socket,message)
-        {:ok, data} = :gen_tcp.recv(socket, 0)
-        IO.puts(data)
-        loop(socket,message,n-1)
-    end
-
 end
