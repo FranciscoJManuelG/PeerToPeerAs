c80a662a00960f0333e66caaa06f31732ce42a27
David García Gondell <david.ggondell@udc.es>
2019-05-15 17:53:50 +0200

    [ADD] admin interface

---
+++ lib/Client/Interface/AdminPeer.ex
@@ -0,0 +1,52 @@
+defmodule AdminPeer do
+
+    defp do_operation(operation,ip,port) do 
+        Client.connect(ip,port)
+        Client.send(operation)
+        Client.close()
+    end
+
+    defp hash(fich) do
+        Enum.join(String.split(Kernel.inspect(
+            [:crypto.hash(:sha256,File.read!(Utils.param(:files)<>fich))]))
+        )
+    end
+    
+    def connect(ip,port) do
+        do_operation("CONNECT",ip,port)
+        case spawn_link(ServerPeer,:accept,[4000]) do
+            {:ok,pid} ->  Process.register(pid, :serverpeer)
+            _ ->"No se pudo iniciar el servidor interno"
+            end
+        :ok
+    end
+
+    def disconnect(ip,port) do
+        do_operation("DISCONNECT",ip,port)
+        # Process.exit(:serverpeer, :normal)
+    end
+
+    def offer(fich,ip,port) do
+        ruta = Utils.param(:files)<>fich
+        case File.exists?(ruta) do
+            true -> do_operation("OFFER "<>fich<>" "<>hash(fich),ip,port)
+            _ -> IO.puts("El fichero no existe")
+        end
+    end
+
+    def want(fich,ip,port) do
+        do_operation("WANT "<>fich,ip,port)
+    end
+
+    def view(ip,port) do
+        do_operation("VIEW",ip,port)
+    end
+
+    def server_stop(ip,port) do
+        do_operation("STOP",ip,port)
+    end
+
+    def give_me_file(ip,fich) do
+        Client.want(String.to_charlist(ip),4000,fich)
+    end
+end
